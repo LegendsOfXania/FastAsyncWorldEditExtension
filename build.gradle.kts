f2a54d9591066245a257579f8a83453882bb4b2a
LegendsOfXania <legendsofxania@gmail.com>
2025-05-04 12:35:06 +0200

    Fix dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
+
 plugins {
     kotlin("jvm") version "2.0.21"
     id("java-library")
     id("com.typewritermc.module-plugin") version "1.1.3"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "fr.xania"
@@ -10,13 +10,10 @@ version = "0.8.0"
 
 repositories {
     mavenCentral()
-    flatDir {
-        dirs("schematic4j")
-    }
 }
 
 dependencies {
-    shadow(project(":schematic4j"))
+    implementation(project(":schematic4j"))
 }
 
 typewriter {
