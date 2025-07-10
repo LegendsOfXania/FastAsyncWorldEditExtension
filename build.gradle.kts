503e98e970e71dc869dee455d452c9d529a645b9
LegendsOfXania <legendsofxania@gmail.com>
2025-07-10 12:05:22 +0200

    Suppot for Typewriter beta 162

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ typewriter {
             |beautiful places directly in Typewriter.
             |Created by the Legends of Xania.
             """.trimMargin()
-        engineVersion = "0.9.0-beta-161"
+        engineVersion = "0.9.0-beta-162"
         channel = com.typewritermc.moduleplugin.ReleaseChannel.BETA
 
         paper {
