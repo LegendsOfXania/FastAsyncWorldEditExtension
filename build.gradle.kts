c87809c96d48a3aa0ca4a6087ba2ea09d9755369
LegendsOfXania <legendsofxania@gmail.com>
2025-10-28 17:53:23 +0100

    Update Typewriter

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     /* paperweight */
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
     /* Typewriter */
-    id("com.typewritermc.module-plugin") version "2.0.0"
+    id("com.typewritermc.module-plugin") version "2.1.0"
 }
 
 repositories {
@@ -12,16 +12,14 @@ repositories {
     maven("https://maven.enginehub.org/repo/")
 }
 
-
 dependencies {
     /* FAWE */
-    implementation(platform("com.intellectualsites.bom:bom-newest:1.52"))
+    implementation(platform("com.intellectualsites.bom:bom-newest:1.55"))
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core")
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit") { isTransitive = false }
     /* paperweight */
     paperweight.paperDevBundle("1.21.8-R0.1-SNAPSHOT")
 }
-
 
 group = "fr.xania"
 version = "0.9.0"
@@ -37,7 +35,7 @@ typewriter {
             |beautiful places directly in Typewriter.
             |Created by the Legends of Xania.
             """.trimMargin()
-        engineVersion = "0.9.0-beta-166"
+        engineVersion = "0.9.0-beta-167"
         channel = com.typewritermc.moduleplugin.ReleaseChannel.BETA
 
         paper {
